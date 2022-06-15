7827bf42f7693c2ea68cb80170080e5e1bea2429
donglinjie <1490767581@qq.com>
2022-06-15 16:14:16 +0800

    写调用方法

--- vehicle-manage/routes/workers.js
+++ vehicle-manage/routes/workers.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
-var api = require('../utils/mysql');
+const express = require('express');
+const router = express.Router();
+const api = require('../utils/mysql');
 
-router.post('/', function(req, res, next) {
+// demo
+/*router.post('/', function(req, res, next) {
   api.HTTP(sql).then(resp => {
     res.status(item.code).send(resp)
+  })
+})*/
+
+// 查询员工信息
+router.get("/query",(req,res,next)=>{
+  let params = req.query;
+  res.json({
+    code: 0,
+    msg: "成功",
+    params
   })
 })
