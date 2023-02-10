dfa00fdc76c2f38cceee214bfed3eaa99649b355
mysmiles <1490767581@qq.com>
2023-02-10 14:21:08 +0800

    worker的增删改查

--- vehicle-manage/lib/worker.js
+++ vehicle-manage/lib/worker.js
@@ -8,33 +8,59 @@ exports.insertData = ( value ) => {
 
 // 添加员工信息
 exports.insertWorkerData = ( value ) => {
-  let _sql = "insert into worker set id=?,name=?,phone=?,sex=?;"
-  const valueArr = Object.values(value)
+  const {id, name = null, phone = null, gender = null, is_delete = 0} = value
+  const valueArr = [id, name, phone, gender, is_delete]
+  let _sql = "insert into worker set id=?,name=?,phone=?,gender=?,is_delete=?;"
   return query( _sql, valueArr )
 }
 
 // 更新员工信息
-exports.updateWorkerData = ( value ) => {
-  let _sql = "update worker set name=?,phone=?,sex=? where id=?;"
-  return query( _sql, value )
+exports.updateWorkerData = (id, value) => {
+  const {name = null, phone = null, gender = null} = value
+  const valueArr = [name, phone, gender, id]
+  let _sql = "update worker set name=?,phone=?,gender=? where id=?;"
+  return query( _sql, valueArr )
 }
 
 // 软删除员工信息
-exports.updateWorkerData = ( value ) => {
-  let _sql = "update worker set is_delete=1 where id=?;"
-  return query( _sql, value )
+exports.deleteWorkerData = ( id ) => {
+  let _sql = `update worker set is_delete=1 where id="${id}";`
+  return query(_sql)
 }
 
-// 删除员工信息
+/* // 删除员工信息
 exports.deleteWorkerData = ( id ) => {
   let _sql = `DELETE FROM worker WHERE id="${id}";`
   return query( _sql )
-}
+} */
 
 // 查询员工信息
 exports.selectWorkerData = ( id ) => {
   let _sql = `select * from worker where id="${id}"`
-  return query( _sql )
+  return query(_sql)
+}
+
+// 查询员工列表
+exports.selectWorkerList = ( value ) => {
+  let { pageSize = 10, pageNo = 0 } = value
+  const keys = ['name', 'phone', 'gender']
+  let condition = ''
+  for (let key in value) {
+    if (value[key] && keys.includes(key)) {
+      condition += `${key}="${value[key]}" and `
+    }
+  }
+  let limitMin = pageSize * pageNo
+  let limitMax = pageSize * (pageNo + 1)
+
+  let _dataSql = `select * from worker where ${condition} is_delete=0 limit ${limitMin}, ${limitMax}`
+  const data = query(_dataSql)
+
+  let _countSql = `select count(*) as total from worker where ${condition} is_delete=0`
+  const count = query(_countSql)
+  console.log(data, count)
+
+  return Promise.all([data, count])
 }
 
 // 查询员工列表
--- vehicle-manage/routes/workers.js
+++ vehicle-manage/routes/workers.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userModel = require('../lib/worker.js')
 
+const { nanoid } = require('nanoid');
+
 // demo
 /*router.post('/', function(req, res, next) {
   api.HTTP(sql).then(resp => {
@@ -19,10 +21,90 @@ const userModel = require('../lib/worker.js')
   })
 })*/
 
+// 添加员工
 router.post("/addWorker",(req,res,next) => {
-  let paramBody = req.query; // body传参
+  const id = nanoid()
+  let paramBody = { ...req.body, id: id }; // body传参
   userModel.insertWorkerData(paramBody).then(resp => {
-    res.status(resp.code).send(resp)
+    res.status(resp.code).send({
+      code: resp.code,
+      msg: '保存成功'
+    })
+  }).catch(error => {
+    res.status(error.code).send({
+      code: error.code,
+      msg: error.errorMsg.message
+    })
+  })
+})
+
+// 修改员工
+router.put('/updateWorker/:workerId', (req, res) => {
+  let workId = req.params.workerId
+  let form = req.body
+
+  userModel.updateWorkerData(workId, form).then(resp => {
+    res.status(resp.code).send({
+      code: resp.code,
+      msg: '保存成功'
+    })
+  }).catch(error => {
+    res.status(error.code).send({
+      code: error.code,
+      msg: error.errorMsg.message
+    })
+  })
+})
+
+// 查询员工
+router.get('/worker/:workerId', (req, res) => {
+  let workId = req.params.workerId
+  userModel.selectWorkerData(workId).then(resp => {
+    let result = resp.data[0]
+    res.status(resp.code).send({
+      code: resp.code,
+      data: result
+    })
+  }).catch(error => {
+    res.status(error.code).send({
+      code: error.code,
+      msg: error.errorMsg.message
+    })
+  })
+})
+
+// 按照条件查询列表
+router.get('/workerList', (req, res) => {
+  let query = req.query
+  userModel.selectWorkerList(query).then(resp => {
+    const data = resp[0].data
+    const totalCount = resp[1].data[0].total
+    res.status(200).send({
+      code: 200,
+      data: data,
+      totalCount: totalCount
+    })
+  }).catch(error => {
+    res.status(error.code).send({
+      code: error.code,
+      msg: error.errorMsg.message
+    })
+  })
+})
+
+// 删除员工
+router.delete('/deleteWorker/:workerId', (req, res) => {
+  let workId = req.params.workerId
+  userModel.deleteWorkerData(workId).then(resp => {
+    res.status(resp.code).send({
+      code: resp.code,
+      msg: '操作成功'
+    })
+  }).catch(error => {
+    res.status(error.code).send({
+      code: error.code,
+      msg: error.errorMsg.message
+    })
   })
 })
 
