cecdda98514d08616d0188f71f13f67e7c334bb1
donglinjie <1490767581@qq.com>
2023-03-03 18:16:57 +0800

    rides

--- vehicle-manage/utils/redis.js
+++ vehicle-manage/utils/redis.js
@@ -1,45 +1,68 @@
+const redis = require('redis');
+const port = 6379;
+const host = "127.0.0.1";
+// 你设置的redis密码
+// const password = 'xxxx';
+
+/**
+ * 设置值,并设置过期时间
+ * @param {String} key 键
+ * @param {String} value 值(Object请使用JSON.stringify()包裹)
+ * @param {Number} expire 过期时间(单位: 秒)
+ */
+/* const redisSet = (key, value, expire) => new Promise((resolve, reject) => {
+  const redisClient = redis.createClient(port, host);
+  // redisClient.auth(password);
+  redisClient.on('connect', () => {
+    redisClient.set(key, value, (err, reply) => {
+      if (err) throw err;
+      else resolve(reply);
+      client.quit();
+    })
+    client.expire(key, expire); // expire 过期时间(秒)
+  })
+}) */
+
 const redis = require('redis') // 引入 redis
 
 const redisClient = redis.createClient() // 创建客户端
+/* createClient({
+  url: 'redis://:123456@127.0.0.1:6379'
+    /*
+    * redis://[[username][:password]@][host][:port][/db-number]
+    * 写密码redis://:123456@127.0.0.1:6379/0
+    * 写用户redis://uername@127.0.0.1:6379/0
+    * 或者不写密码 redis://127.0.0.1:6379/0
+    * 或者不写db_number redis://:127.0.0.1:6379
+    * *
+}); */
 
-// 监听错误信息
+redisClient.on('ready', () => {
+  console.log('redis client ready: ')
+});
+
 redisClient.on('err', err => {
   console.log('redis client error: ', err)
 });
 
-(async () => {
-  await redisClient.connect();
+redisClient.connect().then(r => {
 
-  await redisClient.set('key', 'value').then(val => {
-    console.log(val)
-  });
-  const value = await redisClient.get('key');
-  console.log(value);
-  await redisClient.disconnect();
-})()
+});
 
+class Redis {
+  constructor() {
+  }
+  setValue = async (key, value, expire) => {
+    await redisClient.set(key, value, { EX: expire })
+  }
 
+  getValue = async (key) => {
+    return await redisClient.get(key).then(data => data) || null
+  }
 
-// 连接
-/* redisClient.connect(6379, '127.0.0.1').then(() => {
-  // 新增
-  redisClient.set('name', 'zhangsan')
-      .then(val => {
-        console.log(val)
-      })
+  delValue = async (key) => {
+    return await redisClient.del(key)
+  }
+}
 
-  // 查询
-  redisClient.get('name')
-      .then(val => {
-        console.log(val)
-      })
-
-  // 删除
-  redisClient.del('name')
-      .then(val => {
-        console.log(val)
-      })
-})
-
-redisClient.quit() */
 
---
+++ vehicle-manage/utils/redisDemo.js
@@ -0,0 +1,101 @@
+const redisDemo = require('redis') // 引入 redis
+
+const redisClient = redisDemo.createClient() // 创建客户端
+
+// 监听错误信息
+redisClient.on('err', err => {
+  console.log('redis client error: ', err)
+});
+
+(async () => {
+  await redisClient.connect();
+
+  await redisClient.set('key', 'value').then(val => {
+    console.log(val)
+  });
+  const value = await redisClient.get('key');
+  console.log(value);
+  await redisClient.disconnect();
+})()
+
+
+
+// 连接
+/* redisClient.connect(6379, '127.0.0.1').then(() => {
+  // 新增
+  redisClient.set('name', 'zhangsan')
+      .then(val => {
+        console.log(val)
+      })
+
+  // 查询
+  redisClient.get('name')
+      .then(val => {
+        console.log(val)
+      })
+
+  // 删除
+  redisClient.del('name')
+      .then(val => {
+        console.log(val)
+      })
+})
+
+redisClient.quit() */
+
+/**
+ * 创建类
+ */
+class DateTool {
+  // 构造函数
+  constructor(...args) {
+    console.log('args', args);
+  }
+
+  // 实例方法
+  getWeek() {
+    return new Date().getDay()
+    // console.log('getWeek')
+  }
+
+  getDay() {
+    return new Date().getDate()
+  }
+
+  getWD() {
+    return `周${this.getWeek()} ${this.getDay()}号`
+  }
+
+  // 静态方法 属性 单例模式
+  static defaultInstance() {
+
+    if (!DateTool.instance) {
+      DateTool.instance = new DateTool()
+    }
+
+    return DateTool.instance
+  }
+}
+
+/* let dateTool = new DateTool(1,2,4);
+console.log(dateTool.getWeek()); */
+
+console.log(DateTool.defaultInstance().getWD());
+
+/**
+ * 继承
+ * super 调用父类构造函数
+ */
+
+class TimeTool extends DateTool {
+  constructor(parma = {}) {
+    super(parma);
+  }
+}
+
+/* new TimeTool({key: '1'})
+console.log(TimeTool.defaultInstance().getWD()); */
+
+
+
+
