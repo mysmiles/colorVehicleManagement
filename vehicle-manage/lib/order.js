d9f9cb1194fc6d5540490c56ef9cc6618f7d62b6
donglinjie <17370228297@189.cn>
2023-11-06 18:40:15 +0800

    工单

--- vehicle-manage/lib/order.js
+++ vehicle-manage/lib/order.js
@@ -2,8 +2,8 @@ let query = require('../utils/mysql').HTTP
 
 // 添加店铺信息
 exports.insertOrderData = ( value, userId ) => {
-  const {id, name = null, address = null, longitude = null, latitude = null, manager = null, phone = null, is_delete = 0} = value
-  const valueArr = [id, name, address, longitude, latitude, manager, phone, is_delete, userId]
+  const {id, vehicle = null, type = null, amount = null, work_date = null, source_shop = null, is_delete = 0} = value
+  const valueArr = [id, vehicle, type, amount, work_date, source_shop, is_delete, userId]
   let _sql = "insert into order set id=?,name=?,address=?,longitude=?,latitude=?,manager=?,phone=?,is_delete=?,userId=?;"
   return query( _sql, valueArr )
 }
